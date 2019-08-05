a2513d17bacfa97a41573d591ae7cf829667bda8
cguerin <guerin.claire01@gmail.com>
2019-08-05 15:53:31 +0200

    First expected test fail: need to write functional test!

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,10 @@
-# functional test
+import pytest
 
-# Claire wants to model the evolution of a structured population
-# She provides the initialization specifications for the run: number of demes, initial deme size, number of generations
-# She provides parameters needed by the program to run functions
+# Claire wants to model the evolution of a structured population.
+# She provides the initialization specifications for the run: number of demes, initial deme size, number of generations in a file called initialization.txt
+# She provides parameters needed by the program to run functions in a file called parameters.txt
 # She runs the program
 # After the run, the results are saved in a folder called "res"
 # Satisfied, she goes to sleep.
+
+assert 1==2, "Write the functional test!"
