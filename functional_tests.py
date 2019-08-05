9073fc02d716e0e052979854737f1757818b2ead
cguerin <guerin.claire01@gmail.com>
2019-08-05 15:42:02 +0200

    Wrote the story of a simple functional test for the minimal code to work

---
+++ functional_tests.py
@@ -0,0 +1,8 @@
+# functional test
+
+# Claire wants to model the evolution of a structured population
+# She provides the initialization specifications for the run: number of demes, initial deme size, number of generations
+# She provides parameters needed by the program to run functions
+# She runs the program
+# After the run, the results are saved in a folder called "res"
+# Satisfied, she goes to sleep.
