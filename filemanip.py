b9e025380f34647769e025d68e3dcb97a23118c3
cguerin <guerin.claire01@gmail.com>
2019-08-06 17:33:50 +0200

    adjusted functional tests according to new file manipulation module, added some more functions into the module

--- filemanip.py
+++ filemanip.py
@@ -1,8 +1,17 @@
+import os 
+
 class FileManipulation(object):
 			
-	def getPathToFile(self,filename):
+	def getPathToFile(filename):
 		return "{0}/{1}".format(os.getcwd(), filename)
 	
-	def extractColumnFromFile(self, fileToRead, col, datatype):
+	def searchFile(fileToRead, string):
+		with open(fileToRead) as f:
+			if string in f.read():
+				assert True
+			else:
+				assert False, "{0} not provided in {1}".format(string, fileToRead)		
+	
+	def extractColumnFromFile(fileToRead, col, datatype):
 		with open(fileToRead) as f:
 			return [datatype(line.split(',')[col]) for line in f.readlines()]
--- test/functional_test.py
+++ test/functional_test.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+from filemanip import FileManipulation as fman
 from main import Population as pop
 
 INITIALISATION_FILE = 'initialisation.txt'
@@ -7,13 +8,6 @@ PARAMETER_FILE = 'parameters.txt'
 
 class TestSimpleRun(object):
 	
-	def searchFile(self, fileToRead, string):
-		with open(fileToRead) as f:
-			if string in f.read():
-				assert True
-			else:
-				assert False, "{0} not provided in {1}".format(string, fileToRead)
-				
 	def assertObjectAttributesExist(self, obj, attrs):
 		for attr in attrs:
 			assert hasattr(obj, attr), "object {0} has no attribute {1}".format(obj, attr)	
@@ -21,13 +15,6 @@ class TestSimpleRun(object):
 	def assertObjectAttributeValues(self, obj, attrs, vals):
 		for attr,val in zip(attrs, vals):
 			assert getattr(obj, attr) == val, "{0} has {1}={2} instead of {3}".format(obj, attr, getattr(obj, attr), val)
-			
-	def extractColumnFromFile(self, fileToRead, col, datatype):
-		with open(fileToRead) as f:
-			return [datatype(line.split(',')[col]) for line in f.readlines()]
-		
-	def getPathToFile(self,filename):
-		return "{0}/{1}".format(os.getcwd(), filename)
 	
 	def test_initialisation_file_exists(self):
 		# Claire wants to model the evolution of a structured population.
@@ -37,14 +24,14 @@ class TestSimpleRun(object):
 		assert INITIALISATION_FILE in self.fileslist, "Initialisation file not found in {0}".format(self.dirpath)
 		
 	def test_initialisation_parameters_listed(self):
-		self.pathToFile = self.getPathToFile(INITIALISATION_FILE)
+		self.pathToFile = fman.getPathToFile(INITIALISATION_FILE)
 		for par in ['numberOfDemes','initialDemeSize','numberOfGenerations']:
-			self.searchFile(self.pathToFile, par)
+			fman.searchFile(self.pathToFile, par)
 			
 	def test_initialisation_values_provided(self):
-		self.pathToFile = self.getPathToFile(INITIALISATION_FILE)
+		self.pathToFile = fman.getPathToFile(INITIALISATION_FILE)
 		
-		self.pars = self.extractColumnFromFile(self.pathToFile, 1, int)
+		self.pars = fman.extractColumnFromFile(self.pathToFile, 1, int)
 		for par in self.pars:
 			assert type(par) is int
 			assert par > 0	
@@ -58,9 +45,9 @@ class TestSimpleRun(object):
 		# She runs the program:
 	def test_population_is_initialised_with_right_values(self):
 		# First, the population is initialised according to the initialisation settings
-		self.pathToFile = self.getPathToFile(INITIALISATION_FILE)
-		self.attributeNames = self.extractColumnFromFile(self.pathToFile, 0, str)
-		self.attributeValues = self.extractColumnFromFile(self.pathToFile, 1, int)
+		self.pathToFile = fman.getPathToFile(INITIALISATION_FILE)
+		self.attributeNames = fman.extractColumnFromFile(self.pathToFile, 0, str)
+		self.attributeValues = fman.extractColumnFromFile(self.pathToFile, 1, int)
 		self.runpop = pop()
 		
 		self.assertObjectAttributesExist(self.runpop, self.attributeNames)
