26a1b9808be3272232f510c7a34e118ed890b807
cguerin <guerin.claire01@gmail.com>
2019-08-06 10:24:15 +0200

    expected fail: pars not storedin a dict

--- test/functional_test.py
+++ test/functional_test.py
@@ -11,7 +11,6 @@ class TestSimpleRun(object):
 				assert True
 			else:
 				assert False, "{0} not provided in {1}".format(string, fileToRead)
-		f.close()
 	
 	def test_initialisation_file_exists(self):
 		# Claire wants to model the evolution of a structured population.
@@ -19,10 +18,22 @@ class TestSimpleRun(object):
 		self.dirpath = os.getcwd()
 		self.fileslist = os.listdir(self.dirpath)
 		assert INITIALISATION_FILE in self.fileslist, "Initialisation file not found in {0}".format(self.dirpath)
+		
+	def test_initialisation_parameters_listed(self):
+		self.dirpath = os.getcwd()
 		self.pathToFile = "{0}/{1}".format(self.dirpath, INITIALISATION_FILE)
-		self.searchFile(self.pathToFile,'numberOfDemes')
-		self.searchFile(self.pathToFile,'initialDemeSize')
-		self.searchFile(self.pathToFile,'numberOfGenerations')
+		for par in ['numberOfDemes','initialDemeSize','numberOfGenerations']:
+			self.searchFile(self.pathToFile, par)
+			
+	def test_initialisation_values_provided(self):
+		self.dirpath = os.getcwd()
+		self.pathToFile = "{0}/{1}".format(self.dirpath, INITIALISATION_FILE)
+		with open(self.pathToFile) as f:
+			self.initParDict = f.readline()
+		for par in ['numberOfDemes','initialDemeSize','numberOfGenerations']:
+			assert type(self.initParDict[par]) is str
+			assert self.initParDict[par] > 0
+			
 
 		# She provides parameters needed by the program to run functions in a file called parameters.txt
 		# She runs the program
