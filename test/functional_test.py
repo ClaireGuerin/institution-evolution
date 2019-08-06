959eaa6cf649f163c9b2544879747254530136a8
cguerin <guerin.claire01@gmail.com>
2019-08-06 16:53:16 +0200

    successfully set up test for population initial attributes: expected fail as attribute value is set to non for now, instead of what is provided in the text file

--- test/functional_test.py
+++ test/functional_test.py
@@ -14,13 +14,20 @@ class TestSimpleRun(object):
 			else:
 				assert False, "{0} not provided in {1}".format(string, fileToRead)
 				
-	def checkObjectAttribute(self, obj, attrs):
+	def assertObjectAttributesExist(self, obj, attrs):
 		for attr in attrs:
-			assert hasattr(obj, attr), "object {0} has no attribute {1}".format(obj, attr)		
+			assert hasattr(obj, attr), "object {0} has no attribute {1}".format(obj, attr)	
 			
-	def extractParameters(self, fileToRead, col):
-		with open(self.pathToFile) as f:
-			return [int(line.split(',')[col]) for line in f.readlines()]
+	def assertObjectAttributeValues(self, obj, attrs, vals):
+		for attr,val in zip(attrs, vals):
+			assert getattr(obj, attr) == val, "{0} has {1}={2} instead of {3}".format(obj, attr, getattr(obj, attr), val)
+			
+	def extractColumnFromFile(self, fileToRead, col, datatype):
+		with open(fileToRead) as f:
+			return [datatype(line.split(',')[col]) for line in f.readlines()]
+		
+	def getPathToFile(self,filename):
+		return "{0}/{1}".format(os.getcwd(), filename)
 	
 	def test_initialisation_file_exists(self):
 		# Claire wants to model the evolution of a structured population.
@@ -30,16 +37,14 @@ class TestSimpleRun(object):
 		assert INITIALISATION_FILE in self.fileslist, "Initialisation file not found in {0}".format(self.dirpath)
 		
 	def test_initialisation_parameters_listed(self):
-		self.dirpath = os.getcwd()
-		self.pathToFile = "{0}/{1}".format(self.dirpath, INITIALISATION_FILE)
+		self.pathToFile = self.getPathToFile(INITIALISATION_FILE)
 		for par in ['numberOfDemes','initialDemeSize','numberOfGenerations']:
 			self.searchFile(self.pathToFile, par)
 			
 	def test_initialisation_values_provided(self):
-		self.dirpath = os.getcwd()
-		self.pathToFile = "{0}/{1}".format(self.dirpath, INITIALISATION_FILE)
+		self.pathToFile = self.getPathToFile(INITIALISATION_FILE)
 		
-		self.pars = self.extractParameters(pathToFile, 1)
+		self.pars = self.extractColumnFromFile(self.pathToFile, 1, int)
 		for par in self.pars:
 			assert type(par) is int
 			assert par > 0	
@@ -53,8 +58,13 @@ class TestSimpleRun(object):
 		# She runs the program:
 	def test_population_is_initialised_with_right_values(self):
 		# First, the population is initialised according to the initialisation settings
+		self.pathToFile = self.getPathToFile(INITIALISATION_FILE)
+		self.attributeNames = self.extractColumnFromFile(self.pathToFile, 0, str)
+		self.attributeValues = self.extractColumnFromFile(self.pathToFile, 1, int)
 		self.runpop = pop()
-		self.checkObjectAttribute(self.runpop, ['numberOfDemes','initialDemeSize','numberOfGenerations'])
+		
+		self.assertObjectAttributesExist(self.runpop, self.attributeNames)
+		self.assertObjectAttributeValues(self.runpop, self.attributeNames, self.attributeValues)
 		
 	def test_program_runs_x_generations(self):
 		# Second, the population evolves over x generations following the iteration function
