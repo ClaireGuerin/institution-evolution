b1d93f0dbf56761458725ce52ee86be02b1ef624
ClaireGuerin <guerin.claire01@gmail.com>
2019-08-09 16:01:29 +0200

    ongoing business interrupted by arrival at train station... to be continued

--- lifecycle.py
+++ lifecycle.py
@@ -1,12 +1,20 @@
 from main import Population as pop
+import random as rd
 
 class runOneCycle(object):
 	
 	def __init__(self):
 		self.population = pop()
 	
-	def migration(self):
-		pass
+	def migration(self, individual):
+		migrate = np.random.choice([0,1],1,True,[1-dispersalRate,dispersalRate]) #true stands for replacementself.population.migrationProbability
+		### USE RANDOM here instead!!!
+		if random.randint(0,100) < 36:
+    do_stuff()
+		
+		if migrate:
+			pass
+			
 	
 	def reproduction(self):
 		pass
