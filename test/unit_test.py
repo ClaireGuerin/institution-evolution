a1d23c651bc6489588b9d41c5382da4d0ad4d2d8
ClaireGuerin <guerin.claire01@gmail.com>
2019-08-09 15:40:08 +0200

    added another test before to make sure that population is in the initialisation

--- test/unit_test.py
+++ test/unit_test.py
@@ -12,6 +12,11 @@ class TestLifeCycle(object):
 		for method in self.methods:
 			assert hasattr(lc.runOneCycle, method), "{0} method does not exist".format(method)
 			assert callable(getattr(lc.runOneCycle, method)), "{0} method is not callable".format(method)
+			
+	def test_population_attribute_generated_at_initialisation(self):
+		self.method = 'population'
+		assert hasattr(lc.runOneCycle, self.method), "life cycle does has not been fed a with a {0} to run on".format(self.method)
+		assert callable(getattr(lc.runOneCycle, self.method)), "{0} method is not callable in life cycle".format(self.method)
 	
 	def test_migration_returns_a_destination_deme(self):
 		self.destinationDeme = lc.runOneCycle().migration()
