1029d9d3dc3aef82d121f4553f18c651a5b2a2b3
cguerin <guerin.claire01@gmail.com>
2019-09-10 13:36:56 +0200

    removed dummy fitness function

--- fitness.py
+++ fitness.py
@@ -2,9 +2,6 @@ def pgg(res, **kwargs):
 	f = float(kwargs["fb"] * (res - kwargs["c"] * kwargs["x"][0] ** 2 + kwargs["b"] * kwargs["xmean"][0]) / (1 + kwargs["gamma"] * kwargs["n"]))
 	return max(0., f)
 
-def dummy(a):
-	print('{0}! haha it works! '.format(a))
-
 functions = {}
 for key, value in list(locals().items()):
     if callable(value) and value.__module__ == __name__:
