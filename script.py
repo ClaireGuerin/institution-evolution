580359b8b0a917eca4c53d354e038db7851608e3
ClaireGuerin <guerin.claire01@gmail.com>
2019-09-17 10:50:33 +0200

    update readme and script to run single simulation

--- script.py
+++ script.py
@@ -1,16 +1,25 @@
-import main
+from institutionevolution.population import Population as Pop
 from time import perf_counter
 
-for i in range(9):
-	dispersal = (i + 1) / 10
+## THIS CODE IS TO TEST DIFFERENT DISPERSAL (MIGRATION) RATES IN RANGE [0,1]
+# for i in range(9):
+# 	dispersal = (i + 1) / 10
 
-	for i in range(10):
-		trial = main.Population()
-		trial.migrationRate = dispersal
-		trial.createAndPopulateDemes()
+# 	for i in range(10):
+# 		trial = Pop()
+# 		trial.migrationRate = dispersal
+# 		trial.createAndPopulateDemes()
 
-		tic = perf_counter()
-		trial.runSimulation(outputfile="out-d{0}-{1}.txt".format(trial.migrationRate,i))
-		toc = perf_counter()
+# 		tic = perf_counter()
+# 		trial.runSimulation(outputfile="out-d{0}-{1}.txt".format(trial.migrationRate,i))
+# 		toc = perf_counter()
 
-		print("Ran {0} generations with {1} demes in {2} seconds".format(trial.numberOfGenerations, trial.numberOfDemes, toc - tic))
+# 		print("Ran {0} generations with {1} demes in {2} seconds".format(trial.numberOfGenerations, trial.numberOfDemes, toc - tic))
+
+singlerun = Pop()
+singlerun.createAndPopulateDemes()
+tic = perf_counter()
+singlerun.runSimulation(outputfile="outputtest.txt")
+toc = perf_counter()
+
+print("Ran {0} generations with {1} demes in {2} seconds".format(singlerun.numberOfGenerations, singlerun.numberOfDemes, toc - tic))
