3ba43595a2174d6052cb1703a8d2c6ea85ae2abf
cguerin <guerin.claire01@gmail.com>
2019-09-18 14:31:57 +0200

    issue with the simulation runs sometimes one of the iterations exits with an error while calculating an individual's reproductive value. Error message is due to the multiplication of a float with a NoneType. This hints towards a problem as population dies out

--- script.py
+++ script.py
@@ -2,24 +2,24 @@ from institutionevolution.population import Population as Pop
 from time import perf_counter
 
 ## THIS CODE IS TO TEST DIFFERENT DISPERSAL (MIGRATION) RATES IN RANGE [0,1]
-# for i in range(9):
-# 	dispersal = (i + 1) / 10
+for i in range(9):
+	dispersal = (i + 1) / 10
 
-# 	for i in range(10):
-# 		trial = Pop()
-# 		trial.migrationRate = dispersal
-# 		trial.createAndPopulateDemes()
+	for i in range(10):
+		trial = Pop()
+		trial.migrationRate = dispersal
+		trial.createAndPopulateDemes()
 
-# 		tic = perf_counter()
-# 		trial.runSimulation(outputfile="out-d{0}-{1}.txt".format(trial.migrationRate,i))
-# 		toc = perf_counter()
+		tic = perf_counter()
+		trial.runSimulation(outputfile="test/out-d{0}-{1}.txt".format(trial.migrationRate,i))
+		toc = perf_counter()
 
-# 		print("Ran {0} generations with {1} demes in {2} seconds".format(trial.numberOfGenerations, trial.numberOfDemes, toc - tic))
+		print("Ran {0} generations with {1} demes in {2} seconds".format(trial.numberOfGenerations, trial.numberOfDemes, toc - tic))
 
-singlerun = Pop()
-singlerun.createAndPopulateDemes()
-tic = perf_counter()
-singlerun.runSimulation(outputfile="outputtest.txt")
-toc = perf_counter()
+#singlerun = Pop()
+#singlerun.createAndPopulateDemes()
+#tic = perf_counter()
+#singlerun.runSimulation(outputfile="outputtest.txt")
+#toc = perf_counter()
 
-print("Ran {0} generations with {1} demes in {2} seconds".format(singlerun.numberOfGenerations, singlerun.numberOfDemes, toc - tic))
+#print("Ran {0} generations with {1} demes in {2} seconds".format(singlerun.numberOfGenerations, singlerun.numberOfDemes, toc - tic))
