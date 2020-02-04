940bfe7871229285c3f8dce3fd25c873ff8b703e
cguerin <guerin.claire01@gmail.com>
2020-02-04 21:42:52 +0100

    preparing script to explore effect of policing level on x and n

--- script.py
+++ script.py
@@ -1,30 +1,30 @@
 from institutionevolution.population import Population as Pop
 from time import perf_counter
 
-## THIS CODE IS TO TEST DIFFERENT DISPERSAL (MIGRATION) RATES IN RANGE [0,1]
-# big_tic = perf_counter()
+# THIS CODE IS TO TEST DIFFERENT PARAMETER VALUES
+big_tic = perf_counter()
 
-# for i in range(9):
-# 	dispersal = (i + 1) / 10
+for parval in range(9):
+	policing = (parval + 1) / 10
 
-# 	for i in range(10):
-# 		trial = Pop()
-# 		trial.migrationRate = dispersal
+	for replicate in range(10):
+		trial = Pop()
+		trial.fitnessParameters.update({"p": policing})
 
-# 		tic = perf_counter()
-# 		trial.runSimulation(outputfile="test/out-d{0}-{1}.txt".format(trial.migrationRate,i))
-# 		toc = perf_counter()
+		tic = perf_counter()
+		trial.runSimulation(outputfile="test/out-p{0}-{1}.txt".format(policing,replicate))
+		toc = perf_counter()
 
-# 		print("Ran {0} generations with {1} demes in {2} seconds".format(trial.numberOfGenerations, trial.numberOfDemes, toc - tic))
+		print("Ran {0} generations with {1} demes in {2} seconds".format(trial.numberOfGenerations, trial.numberOfDemes, toc - tic))
 
-# big_toc = perf_counter()
-# print("Ran 9 simulations in {0} seconds".format(big_toc - big_tic))
+big_toc = perf_counter()
+print("Ran 9 simulations in {0} seconds".format(big_toc - big_tic))
 
-# THIS CODE IS TO RUN A SINGLE SIMULATION
+# # THIS CODE IS TO RUN A SINGLE SIMULATION
 
-singlerun = Pop("policingdemog")
-tic = perf_counter()
-singlerun.runSimulation(outputfile="outputtestdemog.txt")
-toc = perf_counter()
+# singlerun = Pop("policingdemog")
+# tic = perf_counter()
+# singlerun.runSimulation(outputfile="outputtestdemog.txt")
+# toc = perf_counter()
 
-print("Ran {0} generations with {1} demes in {2} seconds".format(singlerun.numberOfGenerations, singlerun.numberOfDemes, toc - tic))
+# print("Ran {0} generations with {1} demes in {2} seconds".format(singlerun.numberOfGenerations, singlerun.numberOfDemes, toc - tic))
