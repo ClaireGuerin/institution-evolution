46bea610d0ade572e0389b96f13a1d7baa120457
cguerin <guerin.claire01@gmail.com>
2020-02-06 19:37:14 +0100

    finished sims and plotted res

--- plot.py
+++ plot.py
@@ -6,15 +6,15 @@ import csv
 import os
 
 dirpath = os.getcwd()
-time = range(0,20000)
+time = range(0,10000)
 
 meancoop = []
 varcoop = []
 
-for i in range(9):
-	dispersal = (i + 1) / 10
-	fileslist = glob.glob('{0}/res/test/out-d{1}*'.format(dirpath,dispersal))
-
+for i in range(8):
+	parvalue = (i + 1) / 10
+#	fileslist = glob.glob('{0}/res/test/out-d{1}*'.format(dirpath,dispersal))
+	fileslist = glob.glob('/home/claire/Desktop/pol-demog/rawdat/out-p{0}-[0123456789].txt_d*'.format(parvalue))
 	fig = plt.figure(figsize=(20,10))
 	ax1 = fig.add_subplot(111)
 
@@ -29,15 +29,15 @@ for i in range(9):
 				coop.append(float(row[0]))
 
 		plt.plot(time,coop, label=simfile)
-		dispcoopmean.append(mean(coop[4500:4999]))
-		dispcoopvar.append(variance(coop[4500:4999]))
+		dispcoopmean.append(mean(coop[9000:-1]))
+		dispcoopvar.append(variance(coop[9000:-1]))
 
 	plt.xlabel('Generations')
-	plt.ylabel('Cooperation')
-	plt.ylim(0,1)
-	plt.title('Simulation, dispersal={0}'.format(dispersal))
+	plt.ylabel('Demography')
+	plt.ylim(0,max(coop))
+	plt.title('Simulation, policing={0}'.format(parvalue))
 	plt.legend()
-	plt.savefig('{0}/res/figures/test/plot-d{1}.png'.format(dirpath,dispersal), dpi=300, bbox_inches='tight')
+	plt.savefig('/home/claire/Desktop/pol-demog/figures/plot-demography-p{0}.png'.format(parvalue), dpi=300, bbox_inches='tight')
 	#plt.show()
 
 	meancoop.append(mean(dispcoopmean))
--- plotcomparison.py
+++ plotcomparison.py
@@ -10,32 +10,32 @@ dirpath = os.getcwd()
 fig2 = plt.figure(figsize=(10,10))
 ax2 = fig2.add_subplot(111)
 
-meancoop = [0.5399798487342802, 0.49572097014039923, 0.4419390736126729, 0.45018719325405476, 0.3937387996223269, 0.43575198971908524, 0.42398154934368976, 0.38670825661381936, 0.3864682191449452]
-varcoop = [0.00012708400097636422, 0.00018145909873083866, 0.0001300657737211161, 0.00022490496728090755, 0.0003419391250907176, 0.00015451045782816932, 0.0003494917635666848, 0.00021927794258262272, 0.00018392572703428787]
+meancoop = [10.395527527527527, 10.336311311311311, 10.144686686686686, 10.376505505505506, 10.250788788788789, 10.339353353353353, 10.07926926926927, 10.26939039039039]
+varcoop = [0.11481501050148345, 0.11625390189788988, 0.11968362721438873, 0.11651472368159743, 0.11487569752116847, 0.12079208904295077, 0.11402325842876945, 0.1154559733480976]
 
-x = []
+x = [(i+1)/10 for i in range(8)]
 y = []
 #z = []
 
-with open('/home/claire/institution-evolution/analyticalpredictions.txt','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        x.append(float(row[0])) # dispersal rate
-        y.append(float(row[1])) # cooperation
-        #z.append(float(row[2])) # deme size
+# with open('/home/claire/institution-evolution/analyticalpredictions.txt','r') as csvfile:
+#     plots = csv.reader(csvfile, delimiter=',')
+#     for row in plots:
+#         x.append(float(row[0])) # dispersal rate
+#         y.append(float(row[1])) # cooperation
+#         #z.append(float(row[2])) # deme size
 
 xerror = [0]*len(varcoop)
 yerror = [sqrt(x) for x in varcoop]
 
-plt.plot(x,y, label='Predictions')
+# plt.plot(x,y, label='Predictions')
 plt.plot(x,meancoop, label='Simulations')
 ax2.errorbar(x,meancoop, xerr=xerror, yerr=yerror, fmt='none')
 
-plt.xlabel('Dispersal')
+plt.xlabel('Policing')
 plt.xlim(0,1)
-plt.ylabel('Cooperation')
-plt.ylim(0,1)
+plt.ylabel('Demography')
+plt.ylim(0,max(meancoop)*1.5)
 plt.title('Compare results')
 plt.legend()
-plt.savefig('{0}/res/figures/test/plot-comparison.png'.format(dirpath), dpi=300, bbox_inches='tight')
+plt.savefig('/home/claire/Desktop/pol-demog/figures/plot-comparison-demography.png', dpi=300, bbox_inches='tight')
 plt.show()
