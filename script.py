a449765cb5341b9320366a08d8e1586c6b2639cc
cguerin <guerin.claire01@gmail.com>
2020-02-10 16:48:37 +0100

    nor more replicates in simulations

--- script.py
+++ script.py
@@ -7,15 +7,22 @@ big_tic = perf_counter()
 for parval in range(9):
 	policing = (parval + 1) / 10
 
-	for replicate in range(10):
-		trial = Pop()
-		trial.fitnessParameters.update({"p": policing})
+	# for replicate in range(10):
+	# 	trial = Pop()
+	# 	trial.fitnessParameters.update({"p": policing})
 
-		tic = perf_counter()
-		trial.runSimulation(outputfile="test/out-p{0}-{1}.txt".format(policing,replicate))
-		toc = perf_counter()
+	# 	tic = perf_counter()
+	# 	trial.runSimulation(outputfile="test/out-p{0}-{1}.txt".format(policing,replicate))
+	# 	toc = perf_counter()
 
-		print("Ran {0} generations with {1} demes in {2} seconds".format(trial.numberOfGenerations, trial.numberOfDemes, toc - tic))
+	# 	print("Ran {0} generations with {1} demes in {2} seconds".format(trial.numberOfGenerations, trial.numberOfDemes, toc - tic))
+
+	population = Pop()
+	population.fitnessParameters.update({"p": policing})
+
+	tic = perf_counter()
+	population.runSimulation(outputfile="out-p{0}-{1}.txt".format(policing,replicate))
+	toc = perf_counter()
 
 big_toc = perf_counter()
 print("Ran 9 simulations in {0} seconds".format(big_toc - big_tic))
