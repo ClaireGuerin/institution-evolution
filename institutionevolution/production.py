6023d1626cd698386a657accb176d17a29996baf
ClaireGuerin <guerin.claire01@gmail.com>
2020-07-04 09:00:18 +0200

    need to implement new technology growth function

--- institutionevolution/production.py
+++ institutionevolution/production.py
@@ -14,9 +14,14 @@ def policingdemog2(res, **kwargs):
 	return res
 
 def technology(res, **kwargs):
+	x = kwargs['x'][0]
+	q = kwargs['q']
+	fine = kwargs['pg'] * kwargs['p'] / kwargs['n']
+	benef = (kwargs['pg'] * (1 - kwargs['p']))
 	consensusTime = 1 - kwargs['productionTime']
 	resourcesProduced = (1 - consensusTime) * ((kwargs['n'] * (1 - consensusTime)) ** (-kwargs['alphaResources'])) * kwargs['tech'] ** kwargs['alphaResources']
-	payoff = (1 - kwargs['x'][0]) * (resourcesProduced * (1 - kwargs['q'] * kwargs['d'] * kwargs['p']) - kwargs['q'] * (kwargs['pg'] * kwargs['p'])/kwargs['n'])
+	payoff = (1 - kwargs['x'][0]) * (resourcesProduced * (1 - kwargs['q'] * kwargs['d'] ) - kwargs['q'] * ( * kwargs['p'])/kwargs['n'])
+	payoff = (1-q)*(1-x)*resourcesProduced+q*((1-x)*resourcesProduced-fine) + benef
 	return payoff
 		
 def debate(res, **kwargs):
