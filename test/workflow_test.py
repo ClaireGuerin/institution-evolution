4ce5a081d27b9be6ec3829a9ea08695013765c39
cguerin <guerin.claire01@gmail.com>
2020-07-08 12:29:36 +0200

    expected fail: need to reformat simulation file system

--- test/workflow_test.py
+++ test/workflow_test.py
@@ -1,13 +1,24 @@
 import pytest
+import os
+import sys
+from institutionevolution.population import Population as Pop
 from files import PARAMETER_FOLDER, INITIALISATION_FILE, INITIAL_PHENOTYPES_FILE, INITIAL_TECHNOLOGY_FILE, PARAMETER_FILE, OUTPUT_FOLDER, FITNESS_PARAMETERS_FILE
 
 class TestAutomaticWorkflow(object):
 
+	def test_single_sim_reads_and_writes_from_same_folder(self):
+		self.pop = Pop(inst='test')
+		self.pop.runSimulation()
+
+		assert ["consensus", "demography", "phenotypes", "resources", "technology"] in os.listdir('test')
+
 	def test_script_writes_par_files(self):
-		exec(open("script.py").read())
+		launcher = open("launch_simulations.py").read()
+		sys.argv = ["launch_simulations.py", "arg1", "arg2", "arg3"]
+		exec(launcher)
 
 		self.dirpath = os.getcwd()
-		self.fileslist = os.listdir('{0}/{1}'.format(self.dirpath, PARAMETER_FOLDER))
+		self.fileslist = os.listdir('{0}/{1}/{2}'.format(self.dirpath, PARAMETER_FOLDER, "workflow_test"))
 
 		assert INITIALISATION_FILE in self.fileslist, "{0} not found".format(INITIALISATION_FILE)
 		assert INITIAL_PHENOTYPES_FILE in self.fileslist, "{0} not found".format(INITIAL_PHENOTYPES_FILE)
