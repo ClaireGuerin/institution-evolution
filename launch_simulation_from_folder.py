3223c00d68bada2b4fa26d64b8f50dafa684273c
cguerin <guerin.claire01@gmail.com>
2020-07-16 15:00:09 +0200

    missing info in script, now corrected

--- launch_simulation_from_folder.py
+++ launch_simulation_from_folder.py
@@ -1,4 +1,5 @@
+import sys
 from launch_multiple_simulations import Launcher
 
-l = Launcher(metafolder='doesnamatter', parfile='doesnamatter', launchfile='doesnamatter')
+l = Launcher(metafolder='doesnamatter', parfile='doesnamatter', launchfile=None)
 l.launchSimulation(path=str(sys.argv[1]), mutbound=True)
